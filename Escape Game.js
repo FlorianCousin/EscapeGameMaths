41c818edf103cadebd62b6fb1dcc0abad89818a2
FlorianCousin <florian.cousin@polyconseil.fr>
2021-12-30 23:39:43 +0100

    Mettre le javascript dans un fichier à part

---
+++ Escape Game.js
@@ -0,0 +1,10 @@
+
+function ouverture(enigme) {
+    var overlay = document.getElementById(enigme);
+    overlay.style.display='block';
+}
+
+function fermeture(enigme) {
+    var overlay = document.getElementById(enigme);
+    overlay.style.display='none';
+}
